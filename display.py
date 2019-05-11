41b37a34f8d47bd121dd3c1d7e93eb89611d09fb
Jack53416 <jck53416@gmail.com>
2019-05-11 14:50:37 -0500

    Repair bugs in display module
    - remove bug causing wrapping around x axis even if there was no wrapping
    - add wrapping around y axis
    - now sprites can be removed by drawing them again on the same place
    - correct collision detection

--- display.py
+++ display.py
@@ -13,6 +13,7 @@ class Display(object):
 
     def draw(self, x: int, y: int, sprite: List[int]):
         idx, r = divmod(x, BYTE_SIZE)
+
         mask = 0xFF << (BYTE_SIZE - r) & 0xFF if r > 0 else 0x0
         next_idx = (idx + 1) % self._width
 
@@ -22,19 +23,29 @@ class Display(object):
             raise ValueError("Display Error: Invalid y coordinate")
 
         for sprite_part in sprite:
-            disp_val = (self._data[y][idx] << r) | (self._data[y][next_idx] & mask)
+            disp_val = (self._data[y][idx] << r) | (self._data[y][next_idx] >> (BYTE_SIZE - r))
 
-            disp_val ^= sprite_part
+            disp_val &= sprite_part
 
-            if disp_val != sprite_part:
+            if disp_val != 0:
                 self._collision = True
             else:
                 self._collision = False
 
-            self._data[y][next_idx] |= (disp_val << (BYTE_SIZE - r)) & mask
+            self._data[y][idx] ^= sprite_part >> r
+            self._data[y][next_idx] ^= (sprite_part << (BYTE_SIZE - r)) & 0xFF
 
-            self._data[y][idx] |= disp_val >> r
-            y += 1
+            y = (y + 1) % self._height
+
+        if self.on_draw:
+            self.on_draw()
+
+    def clear(self):
+        self._data = self._data = [bytearray(self._width) for i in range(self._height)]
+
+    @property
+    def collision(self):
+        return self._collision
 
     @property
     def width(self):
@@ -42,7 +53,7 @@ class Display(object):
 
     @property
     def height(self):
-        return  self._height
+        return self._height
 
     @property
     def on_draw(self):
@@ -70,9 +81,27 @@ class Display(object):
             yield str_line
 
 
-if __name__ == "__main__":
-    disp = Display(64, 4)
+def test02(disp: Display):
+    disp.clear()
+    disp.draw(1, 0, [0x80,
+                     0x80,
+                     0x80])
+
+    disp.draw(63, 0, [0x80,
+                      0x80,
+                      0x80])
+    print(disp)
+
+
+def test01(disp: Display):
+    disp.clear()
     disp.draw(59, 0, [0x3C,
                       0xC3,
                       0xFF])
     print(disp)
+
+
+if __name__ == "__main__":
+    disp = Display(64, 4)
+    test01(disp)
+    test02(disp)
